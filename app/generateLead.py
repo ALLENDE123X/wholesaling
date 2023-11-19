ed82417e00ddb33ed830d6a58b752c4313c888fb
awang418 <awang418@gatech.edu>
2023-11-19 04:08:49 -0500

    filter

--- app/generateLead.py
+++ app/generateLead.py
@@ -1,7 +1,7 @@
 import os
 from twilio.rest import Client
 
-def generateleads():
+def generateBuyerLeads():
 
     # Retrieve the values from environment variables
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -12,12 +12,44 @@ def generateleads():
 
     # List of phone numbers and corresponding voicemail URLs
     phone_numbers_and_urls = {
-        '+12403059007': 'https://storage.googleapis.com/wholesaler/output_0.mp3',
-        '+14044446018': 'https://storage.googleapis.com/wholesaler/output_1.mp3',
-        '+16783278767': 'https://storage.googleapis.com/wholesaler/output_2.mp3',
-        '+14046049705': 'https://storage.googleapis.com/wholesaler/output_3.mp3',
-        '+12403059007': 'https://storage.googleapis.com/wholesaler/output_4.mp3',
-        '+14044446018': 'https://storage.googleapis.com/wholesaler/output_5.mp3'
+        '+12403059007': 'https://storage.googleapis.com/wholesaler/output_buyer_0.mp3',
+        '+14044446018': 'https://storage.googleapis.com/wholesaler/output_buyer_1.mp3',
+        '+16783278767': 'https://storage.googleapis.com/wholesaler/output_buyer_2.mp3',
+        '+14046049705': 'https://storage.googleapis.com/wholesaler/output_buyer_3%20(1).mp3',
+        # Add more entries as needed
+    }
+
+    for to_number, voicemail_url in phone_numbers_and_urls.items():
+        twiml = f'''
+        <Response>
+            <Play>{voicemail_url}</Play>
+        </Response>
+        '''
+
+        call = client.calls.create(
+            machine_detection='DetectMessageEnd',
+            twiml=twiml,
+            to=to_number,
+            from_='+18339854826'
+        )
+
+    print(call.sid)
+
+def generatePropertyLeads():
+
+    # Retrieve the values from environment variables
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+    # Create a Twilio client
+    client = Client(account_sid, auth_token)
+
+    # List of phone numbers and corresponding voicemail URLs
+    phone_numbers_and_urls = {
+        '+12403059007': 'https://storage.googleapis.com/wholesaler/output_property_0.mp3',
+        '+14044446018': 'https://storage.googleapis.com/wholesaler/output_property_1.mp3',
+        '+16783278767': 'https://storage.googleapis.com/wholesaler/output_property_2.mp3',
+        '+14046049705': 'https://storage.googleapis.com/wholesaler/output_property_3.mp3',
         # Add more entries as needed
     }
 
--- streamlit.py
+++ streamlit.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import time
 import pandas as pd
-from app.generateLead import generateleads
+from app.generateLead import generatePropertyLeads, generateBuyerLeads
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
 # Load CSV file from the repo
-csv_file_path = "/Users/allisonwang/Documents/Hackathon/wholesaling/ActualPropertyOwnerList.csv"  # Update with the actual path
-properties = pd.read_csv(csv_file_path)
+csv_file_path_properties = "/Users/allisonwang/Documents/Hackathon/wholesaling/ActualPropertyOwnerList.csv"
+properties = pd.read_csv(csv_file_path_properties)
 
-csv_file_path2 = "/Users/allisonwang/Documents/Hackathon/wholesaling/ActualBuyerList.csv"  # Update with the actual path
-buyers = pd.read_csv(csv_file_path2)
+csv_file_path_buyers = "/Users/allisonwang/Documents/Hackathon/wholesaling/ActualBuyerList.csv"
+buyers = pd.read_csv(csv_file_path_buyers)
 
 def simulate_backend_processing(progress_bar, display_text):
-    generateleads()
     progress_bar.progress(0)
     for i in range(1, 101):
         time.sleep(0.1)  # Simulating some backend processing time
@@ -28,6 +33,10 @@ def main():
     # Properties Tab
     if selected_tab == "Properties":
         st.header("Properties List")
+
+        # Add filtering UI
+        global properties  # Ensure 'properties' is treated as a global variable
+        properties = filter_dataframe(properties)
         st.write(properties)
 
         st.subheader("Voicemail Message for Property Owners:")
@@ -40,11 +49,16 @@ def main():
 
         # Simulate backend processing on button click
         if button_properties:
+            generatePropertyLeads()
             simulate_backend_processing(progress_bar_properties, display_text_properties)
 
     # Buyers Tab
     elif selected_tab == "Buyers":
         st.header("Buyers List")
+
+        # Add filtering UI
+        global buyers  # Ensure 'buyers' is treated as a global variable
+        buyers = filter_dataframe(buyers)
         st.write(buyers)
 
         st.subheader("Voicemail Message for Buyers:")
@@ -57,7 +71,67 @@ def main():
 
         # Simulate different backend processing on button click
         if button_buyers:
+            generateBuyerLeads()
             simulate_backend_processing(progress_bar_buyers, display_text_buyers)
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
 
 if __name__ == "__main__":
     main()
