2e73a8ea35667e09c27a72aebb258cceaddeb5a2
William Swann <135576368+rocketboy703@users.noreply.github.com>
2023-11-18 22:31:20 -0500

    Update generateLead.py

--- app/generateLead.py
+++ app/generateLead.py
@@ -8,11 +8,15 @@ auth_token = os.environ['TWILIO_AUTH_TOKEN']
 # Create a Twilio client
 client = Client(account_sid, auth_token)
 
-# Make a Twilio call
-call = client.calls.create(
-    twiml='<Response><Say>Ahoy, World!</Say></Response>',
-    to='+12403059007',  # Replace with the recipient's phone number
-    from_='+18339854826'  # Replace with your Twilio phone number
-)
+# List of phone numbers to call
+phone_numbers = ['+12403059007', '+14044446018']
 
-print(call.sid)
+for to_number in phone_numbers:
+    # Make a Twilio call for each phone number
+    call = client.calls.create(
+        twiml='<Response><Say>Ahoy, World!</Say></Response>',
+        to=to_number,
+        from_='+18339854826'  # Replace with your Twilio phone number
+    )
+
+    print(f"Call SID for {to_number}: {call.sid}")
